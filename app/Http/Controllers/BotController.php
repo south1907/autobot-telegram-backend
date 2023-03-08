4d84f5832e51e24b848fd65a5dbf55159e8d7688
Heva <demo.south.1907@gmail.com>
2023-03-09 01:42:50 +0700

    fix replace

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -112,8 +112,8 @@ class BotController extends Controller
             if ($message->getType() == 'CALLBACK') {
                 $dataCallback = $message->getDataCallback();
                 if (str_contains($dataCallback, 'youtube.com') || str_contains($dataCallback, 'youtu.be')) {
-                    $dataCallback = str_replace($dataCallback, '&feature=share', '');
-                    $dataCallback = str_replace($dataCallback, 'youtu.be/', 'youtube.com/watch?v=');
+                    $dataCallback = str_replace('&feature=share', '', $dataCallback);
+                    $dataCallback = str_replace('youtu.be/', 'youtube.com/watch?v=', $dataCallback);
 
                     // check exists
                     $checkExists = Item::where('link', $dataCallback)->first();
